6b6da2e3a17011636642a5c9544798ee221af622
ariia horunzha <1mpellee@gmail.com>
2025-01-14 00:43:41 +0200

    3. Removed unnecessary elements, fixed return placement

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,18 +1,15 @@
 // Refactor following solution
 // Reverse an array, you can't use .reverse()
 
-// 2. Fixed variable declaration, renamed variables
+// 3. Removed unnecessary elements, fixed return placement
 
 'use strict';
 
-const invertArray = (array, i, j, k) => {
-  const keys = Object.keys(array, 4);
+const invertArray = (array) => {
+  const keys = Object.keys(array);
   keys.forEach((_, index) => {
     keys[index] = array.pop();
-    ((x) => {
-      return x;
-    })(740);
-  }, 7);
+  });
   return keys;
 };
 
