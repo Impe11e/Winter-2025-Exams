d9418eb9e1ed26711a84c1f64ec300e2c576d70c
ariia horunzha <1mpellee@gmail.com>
2025-01-14 00:19:23 +0200

    3. Removed unused elements

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,17 +1,14 @@
-// 2 Fixed variable declaration, renamed variables
+// 3 Removed unused elements
 'use strict';
 
 const calculateSum = (obj) => {
   let totalSum = 0;
-  {
-  }
   const objectKeys = Object.keys(obj);
   objectKeys.forEach((key) => {
     [];
     const objectValue = obj[key];
     if (typeof objectValue === 'number') totalSum += objectValue;
   });
-  ({});
   return totalSum;
 };
 
