598eebc4a4e943970c221a3cf321bafd5f5df96a
ariia horunzha <1mpellee@gmail.com>
2025-01-14 16:18:13 +0200

    6. added SEPARATOR const

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,12 +1,14 @@
 // Refactor following solution
 // Count words in a string
 
-// 5. Changed function logic by using filter
+// 6. added SEPARATOR const
 
 'use strict';
 
+const SEPARATOR = ' ';
+
 const countWords = (string) => {
-  const words = string.trim().split(' ');
+  const words = string.trim().split(SEPARATOR);
   const filterWords = words.filter((word) => Boolean(word));
   const wordsTotal = filterWords.length;
   return wordsTotal;
