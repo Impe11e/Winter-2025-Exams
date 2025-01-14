c3e095163cb2daeb362b509dca604e836e3b8729
ariia horunzha <1mpellee@gmail.com>
2025-01-14 14:31:13 +0200

    3. Moved the array size into a separate variable

--- Tasks/range.js
+++ Tasks/range.js
@@ -1,14 +1,15 @@
 // Refactor following solution
 // Generate int array from given range
 
-// 2. Fixed variable declaration, renamed variables
+// 3. moved the array size into a separate variable
 
 'use strict';
 
 const convertRange = (...range) => {
   const [from, to] = range;
   if (to >= from) {
-    const newRange = new Array(to - from + 1);
+    const arraySize = to - from + 1;
+    const newRange = new Array(arraySize);
     for (let currentNum = from; currentNum <= to; currentNum++) {
       newRange[currentNum - from] = currentNum;
     }
