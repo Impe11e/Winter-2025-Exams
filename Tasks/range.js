c20073d766cddd379963ab6451a7a6ca8e714356
ariia horunzha <1mpellee@gmail.com>
2025-01-14 14:22:13 +0200

    1. Used linter, prettier, added 'use strict'

--- Tasks/range.js
+++ Tasks/range.js
@@ -1,7 +1,11 @@
 // Refactor following solution
 // Generate int array from given range
 
-_range = (...Range) => {
+// 1. Used linter, prettier, added 'use strict'
+
+'use strict';
+
+convertRange = (...Range) => {
   let [from, to] = Range;
   if (to >= from) {
     Range = new Array(to - from + 1);
