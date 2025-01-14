378f8f8f4a621d11bacd8a252be5ad7dc55f7940
ariia horunzha <1mpellee@gmail.com>
2025-01-14 16:16:53 +0200

    5. Changed function logic by using filter

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,25 +1,14 @@
 // Refactor following solution
 // Count words in a string
 
-// 4. Removed unnessesary if-statement logic
+// 5. Changed function logic by using filter
 
 'use strict';
 
 const countWords = (string) => {
-  let wordsTotal = 0;
-  let inWord = false;
-  for (const char of string) {
-    if (!inWord) {
-      if (char === ' ') {
-        inWord = false;
-      } else {
-        inWord = true;
-        wordsTotal++;
-      }
-    } else if (char === ' ') {
-      inWord = false;
-    }
-  }
+  const words = string.trim().split(' ');
+  const filterWords = words.filter((word) => Boolean(word));
+  const wordsTotal = filterWords.length;
   return wordsTotal;
 };
 
